0c3fbb0111d77381c9e0abc551defb69b5e48edb
alessiaacavallari <151541813+alessiaacavallari@users.noreply.github.com>
2024-06-25 16:33:59 +0200

    🧱 Creazione pagina faq 🧱
    - È stata creata la pagina faq collegata al footer

--- script.js
+++ script.js
@@ -1,5 +1,24 @@
+const items = document.querySelectorAll('.accordion button');
+
 function openPopup() {
-    window.location.hash = 'openModal';
+  window.location.hash = 'openModal';
+}
+window.onload = openPopup;
+
+// Utilizzo il js per la sezione faq
+function toggleAccordion() {
+  const itemToggle = this.getAttribute('aria-expanded');
+
+  // Ciclo per ogni elemento e setto l'attributo aria-expanded a false
+  for (i = 0; i < items.length; i++) {
+    items[i].setAttribute('aria-expanded', 'false');
   }
-  
-  window.onload = openPopup;
+
+  // Se clicco su un elemento della lista dei faq si apre e chiude gli altri
+  if (itemToggle == 'false') {
+    this.setAttribute('aria-expanded', 'true');
+  }
+}
+
+// Richiamo la funzione toggleAccordion se viene cliccato un elemento
+items.forEach((item) => item.addEventListener('click', toggleAccordion));
